e5e3fee9aae6013eeaa23b259243b95b22b94ab9
claudiuvilau <64689038+claudiuvilau@users.noreply.github.com>
2024-04-20 17:24:12 +0200

    first commit

---
+++ src/main/java/eu/homeholiday/iifir/IifirApplication.java
@@ -0,0 +1,13 @@
+package eu.homeholiday.iifir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IifirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IifirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/homeholiday/iifir/IifirApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.homeholiday.iifir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IifirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
